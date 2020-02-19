bfa1036080f52bfbe4288b8ed1e1d471656a8e3e
Thaís Oliveira <joliveira>
2020-02-19 10:17:28 -0300

    Secao 14: Exemplo de DataContract

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/BookVO.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/BookVO.cs
@@ -1,13 +1,20 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace RestWithASPNETUdemy.Model
 {
+    //[DataContract]
     public class BookVO
     {
+        //[DataMember (Order = 1, Name = "codigo")]
         public long? Id { get; set; }
+        //[DataMember(Order = 2)]
         public string Title { get; set; }
+        //[DataMember(Order = 3)]
         public string Author { get; set; }
+        //[DataMember(Order = 4)]
         public decimal Price { get; set; }
+        //[DataMember(Order = 5)]
         public DateTime LaunchDate { get; set; }
     }
 }
